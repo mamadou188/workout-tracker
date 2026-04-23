ab64c08cc64aa7ddd981198139924c2629f07cc6
mamadou188 <mamadouseydoutraore38@gmail.com>
2026-04-23 21:50:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,27 @@
-const CACHE = 'workout-log-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE = 'workout-log-v3';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
+// Network first, fall back to cache
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      return cached || fetch(e.request).then(response => {
-        return caches.open(CACHE).then(c => {
-          c.put(e.request, response.clone());
-          return response;
-        });
-      }).catch(() => cached);
-    })
+    fetch(e.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
